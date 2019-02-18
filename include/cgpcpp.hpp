039f3cef6b75da742355a0be6702348580c4cc30
degski <degski@gmail.com>
2019-02-18 08:32:22 +0200

    sax/stl.hpp

--- include/cgpcpp.hpp
+++ include/cgpcpp.hpp
@@ -371,7 +371,7 @@ struct Node {
         actArity { params.arity } {
 
         inputs.reserve ( params.arity );
-        std::generate_n ( stl::back_emplacer ( inputs ), params.arity, [ nodePosition_ ] { return params.getRandomNodeInput ( nodePosition_ ); } );
+        std::generate_n ( sax::back_emplacer ( inputs ), params.arity, [ nodePosition_ ] { return params.getRandomNodeInput ( nodePosition_ ); } );
     }
 
     [[ maybe_unused ]] Node & operator = ( const Node & ) = default;
@@ -407,10 +407,10 @@ struct Chromosome {
         generation { 0 } {
 
         nodes.reserve ( params.numNodes );
-        std::generate_n ( stl::back_emplacer ( nodes ), params.numNodes, [ this ] { return Node<Real> { nodes.size ( ) }; } );
+        std::generate_n ( sax::back_emplacer ( nodes ), params.numNodes, [ this ] { return Node<Real> { nodes.size ( ) }; } );
 
         outputNodes.reserve ( params.numOutputs );
-        std::generate_n ( stl::back_emplacer ( outputNodes ), params.numOutputs, [ ] { return params.getRandomChromosomeOutput ( ); } );
+        std::generate_n ( sax::back_emplacer ( outputNodes ), params.numOutputs, [ ] { return params.getRandomChromosomeOutput ( ); } );
 
         activeNodes.reserve ( params.numNodes );
         setChromosomeActiveNodes ( );
--- include/stl.hpp
+++ include/stl.hpp
@@ -41,58 +41,10 @@
 #include <cereal/types/vector.hpp>
 #endif
 
+#include <sax/stl.hpp>
 
-// STL-like-type functions and classes.
 
 namespace stl {
-
-template<typename ... Ts>
-struct overloaded : Ts... {
-    using Ts::operator ( ) ...;
-};
-template<typename ... Ts>
-overloaded ( Ts ... )->overloaded<Ts ...>;
-
-
-template<typename Container>
-class back_emplace_iterator : public std::iterator<std::output_iterator_tag, void, void, void, void> {
-
-    public:
-
-    using container_type = Container;
-    using value_type = typename Container::value_type;
-
-    protected:
-
-    Container * m_container;
-
-    public:
-
-    explicit back_emplace_iterator ( Container & x ) noexcept : m_container ( & x ) { }
-
-    [[ maybe_unused ]] back_emplace_iterator & operator = ( value_type && t ) {
-        m_container->emplace_back ( std::forward<value_type> ( t ) );
-        return * this;
-    }
-
-    [[ maybe_unused ]] back_emplace_iterator & operator * ( ) { return * this; }
-    [[ maybe_unused ]] back_emplace_iterator & operator ++ ( ) { return * this; }
-    [[ maybe_unused ]] back_emplace_iterator & operator ++ ( int ) { return * this; }
-};
-
-template<typename Container>
-[[ nodiscard ]] back_emplace_iterator<Container> back_emplacer ( Container & c ) {
-    return back_emplace_iterator<Container> ( c );
-}
-
-
-template<typename Container, typename T = typename Container::value_type>
-[[ nodiscard ]] T median ( const Container & container_ ) {
-    Container copy { container_ };
-    auto median = std::next ( std::begin ( copy ), copy.size ( ) / 2 );
-    std::nth_element ( std::begin ( copy ), median, std::end ( copy ) );
-    return *median;
-}
 
 #if defined ( USE_PECTOR )
 template<typename T>
@@ -102,26 +54,5 @@ using vector = pt::pector<T, pt::malloc_allocator<T, true, false>, int, pt::defa
 template<typename T>
 using vector = std::vector<T>;
 #endif
-
-
-// A pair<> that is is_trivially_copyable and therefor faster than std::pair<>.
-// https://www.reddit.com/r/cpp/comments/ar4ghs/stdpair_disappointing_performance/
-
-template<typename KeyType, typename ValueType>
-struct pair {
-    using first_type = KeyType;
-    using second_type = ValueType;
-
-    pair ( ) = default;
-
-    template<typename T1, typename T2>
-    pair ( T1 && key, T2 && value ) :
-        first ( std::forward<T1> ( key ) ),
-        second ( std::forward<T2> ( value ) ) {
-    }
-
-    KeyType first;
-    ValueType second;
-};
 
 };
